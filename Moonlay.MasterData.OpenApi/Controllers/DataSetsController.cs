592397c8cef1340084154e67d86372873152709a
Sjafrudin Lamusu <afandy.lamusu@yahoo.com>
2020-01-27 10:14:19 +0700

    add mdm-app-chart

--- Moonlay.MasterData.OpenApi/Controllers/DataSetsController.cs
+++ Moonlay.MasterData.OpenApi/Controllers/DataSetsController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moonlay.MasterData.OpenApi.Clients;
-using Moonlay.MasterData.OpenApi.Controllers.DTO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
